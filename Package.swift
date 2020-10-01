575890240e9522f65de56cd1a60c02d7b0a5e4fd
Curlon Phillips <39802748+fanz1st@users.noreply.github.com>
2020-10-01 18:03:30 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,6 +7,12 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyDropbox",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
     products: [
         .library(name: "SwiftyDropbox", targets:["SwiftyDropbox"]),
     ],
